8a4e1eefdb2ede32fd8c141a293ad54f9a4eaea1
Utshas <tazwarutshas@gmail.com>
2019-02-11 11:36:17 +0900

    commited everything

---
+++ app/src/main/java/com/example/xmlparse/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.xmlparse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ListView
+import android.widget.SimpleAdapter
+import org.w3c.dom.Element
+import org.w3c.dom.Node
+import org.xml.sax.SAXException
+import java.io.IOException
+import javax.xml.parsers.DocumentBuilderFactory
+import javax.xml.parsers.ParserConfigurationException
+
+class MainActivity : AppCompatActivity() {
+
+    var empDataHashMap = HashMap<String, String>()
+    var empList: ArrayList<HashMap<String,String>> = ArrayList()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        try{
+            val lv =findViewById<ListView>(R.id.listView)
+            val istream = assets.open("empdetail.xml")
+            val builderFactory = DocumentBuilderFactory.newInstance()
+            val docBuilder = builderFactory.newDocumentBuilder()
+            val doc = docBuilder.parse(istream)
+            // reading employee tags
+            val nList = doc.getElementsByTagName("employee")
+            for(i in 0 until nList.length)
+            {
+                if(nList.item(0).nodeType.equals(Node.ELEMENT_NODE))
+                {
+                    empDataHashMap = HashMap()
+                    val element = nList.item(i) as Element
+                    empDataHashMap.put("name", getNodeValue("name",element))
+                    empDataHashMap.put("name", getNodeValue("name",element))
+                    empDataHashMap.put("name", getNodeValue("name",element))
+
+                    empList.add(empDataHashMap)
+                }
+            }
+            val adapter = SimpleAdapter(this@MainActivity, empList, R.layout.custom_list, arrayOf("name", "salary", "designation"), intArrayOf(R.id.name, R.id.salary, R.id.designation))
+            lv.setAdapter(adapter)
+        }
+        catch (e: IOException){
+            e.printStackTrace()
+        }
+         catch (e: ParserConfigurationException) {
+            e.printStackTrace()
+        } catch (e: SAXException) {
+            e.printStackTrace()
+        }
+}
+
+    //return node value
+    protected fun getNodeValue(tag: String, element: Element): String{
+        val nodeList = element.getElementsByTagName(tag)
+        val node = nodeList.item(0)
+        if(node != null){
+            if(node.hasChildNodes()){
+                val child = node.firstChild
+                while(child!=null){
+                    if(child.nodeType === org.w3c.dom.Node.TEXT_NODE)
+                    {
+                        return child.nodeValue
+                    }
+                }
+            }
+        }
+        return ""
+    }
+}
